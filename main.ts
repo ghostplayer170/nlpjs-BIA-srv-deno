726a3e7dbfddb012d6f70f7261b9fe26fde6344c
Renato A Montenegro Palma <ramontenegropalma@gmail.com>
2024-10-25 12:19:11 +0200

    Refactor main.ts to improve code structure and readability

--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
-import { handleNLPRequest } from "./routes/nlpRouter.ts";
-import { trainModel } from "./services/nlpService.ts"; // Importar la función de entrenamiento
+import { handleNLPRequest } from './routes/nlpRouter.ts';
 
-// Entrenar el modelo NLP al iniciar el servidor
-trainModel().then(() => {
-  console.log("NLP Model trained and server is ready.");
+// Configurar el puerto
+const port = 3000;
+console.log(`NLP.js server running on http://localhost:${port}`);
 
-  // Configurar el puerto
-  const port = 3000;
-  console.log(`NLP.js server running on http://localhost:${port}`);
+// Servir las solicitudes usando Deno.serve
+Deno.serve({ port }, async (req: Request) => {
+  const { pathname } = new URL(req.url);
 
-  // Servir las solicitudes
-  Deno.serve({ port }, async (req) => {
-    if (req.method === "POST" && new URL(req.url).pathname === "/nlp") {
-      return await handleNLPRequest(req); // Maneja las solicitudes POST a /nlp
-    } else {
-      return new Response("Not Found", { status: 404 });
-    }
-  });
+  if (req.method === "POST" && pathname === "/nlp") {
+    // Manejar la solicitud POST que llega a /nlp
+    return await handleNLPRequest(req);
+  } else {
+    // Devolver una respuesta 404 para cualquier otra ruta
+    return new Response("Not Found", { status: 404 });
+  }
 });
